d7811573501c91d8267a6709e6fd392b3b2fea6e
Charlie Hastings <hasti123@me.com>
2020-11-11 22:34:10 -0600

    add ranked results

--- generate_pools.py
+++ generate_pools.py
@@ -1,5 +1,51 @@
 import json
 
+
+class Player:
+    def __init__(self, name, GWR, OWGR):
+        self.name = name
+        self.GWR = GWR
+        self.OWGR = OWGR
+
+
+class Group:
+    def __init__(self, playerA, playerB, playerC, playerD, groupOWGR, groupGWR):
+        self.playerA = playerA
+        self.playerB = playerB
+        self.playerC = playerC
+        self.playerD = playerD
+        self.groupOWGR = groupOWGR
+        self.groupGWR = groupGWR
+
+
+def writeGroupsToFile(file_name, groups):
+    with open(file_name, 'w') as f:
+        for group in groups:
+            group_str = 'A: ' + group.playerA.name + ' B: ' + group.playerB.name + ' C: ' + group.playerC.name + \
+                ' D: ' + group.playerD.name + ' OWGR: ' + \
+                str(group.groupOWGR) + ' GWR: ' + str(group.groupGWR)
+            f.write("%s\n" % group_str)
+
+
+def playerDecoder(raw_players):
+    players = []
+    for player in raw_players:
+        players.append(Player(player['name'], player['GWR'], player['OWGR']))
+    return players
+
+
+def get_group_gwr(group):
+    return group.groupGWR
+
+
+def get_group_owgr(group):
+    return group.groupOWGR
+
+
+def get_group_total_rank(group):
+    return group.groupOWGR + group.groupGWR
+
+
 # Opening JSON file
 f = open('data.json')
 
@@ -7,10 +53,10 @@ f = open('data.json')
 # a dictionary
 data = json.load(f)
 
-groupA = data['groupA']
-groupB = data['groupB']
-groupC = data['groupC']
-groupD = data['groupD']
+groupA = playerDecoder(data['groupA'])
+groupB = playerDecoder(data['groupB'])
+groupC = playerDecoder(data['groupC'])
+groupD = playerDecoder(data['groupD'])
 
 # Closing file
 f.close()
@@ -22,9 +68,21 @@ for playerA in groupA:
     for playerB in groupB:
         for playerC in groupC:
             for playerD in groupD:
-                results.append('A: ' + playerA['name'] + ' B: ' + playerB['name'] +
-                               ' C: ' + playerC['name'] + ' D: ' + playerD['name'])
+                groupOWGR = playerA.OWGR + playerB.OWGR + playerC.OWGR + playerD.OWGR
+                groupGWR = playerA.GWR + playerB.GWR + playerC.GWR + playerD.GWR
+                results.append(Group(playerA, playerB, playerC,
+                                     playerD, groupOWGR, groupGWR))
 
-with open('results.txt', 'w') as f:
-    for group in results:
-        f.write("%s\n" % group)
+writeGroupsToFile('unranked_results.txt', results)
+
+results.sort(key=get_group_owgr)
+
+writeGroupsToFile('owgr_results.txt', results)
+
+results.sort(key=get_group_owgr)
+
+writeGroupsToFile('gwr_results.txt', results)
+
+results.sort(key=get_group_total_rank)
+
+writeGroupsToFile('total_rank_results.txt', results)
